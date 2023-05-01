a13a274554f77a037f12c700afa43f948158b6ff
Farouq Haider <ekkarona@gmail.com>
2023-05-02 00:01:26 +0500

    Final Commit Before Lab. Check new php file

--- js/breadcrumbs.js
+++ js/breadcrumbs.js
@@ -1,13 +1,14 @@
 links = document.getElementsByTagName("a");
-if (links[0].href.endsWith("#")) {
+console.log(links);
+if (links[1].href.endsWith("#")) {
   document.getElementById("breadcrumbs").innerHTML =
     "<span>Home</span> / <span>Artists</span>";
   console.log("executed");
-} else if (links[1].href.endsWith("#")) {
+} else if (links[2].href.endsWith("#")) {
   document.getElementById("breadcrumbs").innerHTML =
     "<span>Home</span> / <span>Gallery</span>";
   console.log("executed");
-} else if (links[2].href.endsWith("#")) {
+} else if (links[3].href.endsWith("#")) {
   document.getElementById("breadcrumbs").innerHTML =
     "<span>Home</span> / <span>About</span>";
   console.log("executed");
